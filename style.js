101185235f4abaef0f0c37d7abb1ff3014e1d649
Lucas Malta <lucas50xd@hotmail.com>
2017-12-06 12:39:41 -0200

    initial commit

---
+++ style.js
@@ -0,0 +1,38 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    main: {
+        display: "flex",
+        backgroundColor: "#013243",
+        flex: 1
+    },
+    input: {
+        color: "white"
+    },
+    title: {
+        color: "white",
+        fontSize: 60,
+        alignSelf: "center",
+        marginBottom: "15%"
+    },
+    view: {
+        justifyContent: "center",
+        flex: 9
+    },
+    item: {
+        width: "80%",
+        marginLeft: "auto",
+        marginRight: "auto"
+    },
+    registerButton: {
+        marginTop: "15%",
+        alignSelf: "center",
+        alignItems: "center",
+        width: "90%"
+    },
+    icon : {
+        color: "#FF5050",
+        fontSize: 80,
+        alignSelf: "center"
+    }
+});
